31bbcb86fbd5eeb4b647420a6f287b5326d6c643
Nithikesh <nithikeshr@gmail.com>
2021-11-11 13:48:54 +0530

    pattern task

---
+++ Demo.java
@@ -0,0 +1,45 @@
+package tamilnadu.chennai;
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+	
+		for(int end=1; end<=5;end++ )
+		{
+				for (int val=5; val>=end;val--)
+		{
+			System.out.print("*");
+		}
+				System.out.println();
+				
+		}
+
+				int value=5;
+				for(int end=1;end<=value;end++)
+				{
+				for (int val=end;val<=value;val++)
+				{
+					System.out.print(val+"");
+				}
+				
+				System.out.println();
+				
+				}
+			
+				int value1=1;
+				for(int end=5;end>=value1;end--)
+				{
+				for (int val=end;val>=value1;val--)    
+				{
+					System.out.print(val+"");	 
+				}
+				
+				System.out.println();
+				
+				}
+
+		}
+
+
+		}
---
+++ Demo6.java
@@ -0,0 +1,133 @@
+package tamilnadu.chennai;
+
+public class Demo6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int end=1; end<=5;end++ )
+{
+	for(int i=1; i<=end; i++)
+	{
+		System.out.print(i+" ");
+	}
+	System.out.println();
+}
+
+for(int end=5; end>=1;end-- )
+{
+	for(int i=5; i>=end; i--)
+	{
+		System.out.print(i+" ");
+	}
+	System.out.println();
+} 
+
+for(int i=5;i>=1;i--)  
+{ 
+	for(int j=i;j<=5;j++)  
+	{  
+		System.out.print(j+" ");  
+	}  
+	System.out.println();
+
+}
+for(int i=5;i>=1;i--)  
+{ 
+	for(int j=5;j>=i;j--)  
+	{  
+		System.out.print(i+" ");  
+	}  
+	System.out.println();
+}
+for(int i=1;i<=5;i++)  
+{ 
+	for(int j=1;j<=i;j++)  
+	{  
+		System.out.print(i+" ");  
+	}
+System.out.println();
+}
+
+for(int end=1; end<=5;end++ )
+{
+	for(int i=1; i<=end; i++)
+	{
+		System.out.print(i+" ");
+	}
+	for(int a=end;a<=5;a++) 
+	{
+	System.out.print("*"+" ");
+	}
+	System.out.println();
+}
+
+for(int end=5; end>=1;end-- )
+{
+	for(int i=5; i>=end; i--)
+	{
+		System.out.print(i+" ");
+	}
+	for(int a=end;a>=1;a--) 
+	{
+	System.out.print("*"+" ");
+	}
+	System.out.println();
+} 
+
+for(int i=5;i>=1;i--)  
+{ 
+	for(int j=i;j<=5;j++)  
+	{  
+		System.out.print(j+" ");  
+	}
+	for(int a=i;a>=1;a--) 
+	{
+	System.out.print("*"+" ");
+	}
+	System.out.println();
+
+}
+
+for(int i=5;i>=1;i--)  
+{ 
+	for(int j=5;j>=i;j--)  
+	{  
+		System.out.print(i+" ");  
+	}  
+	for(int a=i;a>=1;a--) 
+	{
+	System.out.print("*"+" ");
+	}
+	System.out.println();
+}
+for(int i=1;i<=5;i++)  
+{ 
+	for(int j=1;j<=i;j++)  
+	{  
+		System.out.print(i+" ");  
+	}
+	for(int a=i;a<=5;a++) 
+	{
+	System.out.print("*"+" ");
+	}
+System.out.println();
+}
+
+
+for(int i=1;i<=5;i++)  
+{ 
+	for(int j=1;j<=i;j++)  
+	{  
+	System.out.print(i+" ");  
+	}
+	for(int a=i;a<=5;a++) 
+	{
+	System.out.print("*"+" ");
+	}
+    System.out.println();
+	
+
+}
+}
+	}
+	
